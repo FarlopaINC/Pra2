7a47a182764894e375bc47f3f304441b477b94a1
Mario <marionten05@gmail.com>
2023-10-01 12:31:45 +0200

    first commit

---
+++ makefile
@@ -0,0 +1,4 @@
+OPTS = -Wall -Wextra -O3 -std=c++0x
+
+tarea2: tarea2.cpp
+	g++ ${OPTS} -o tarea2 tarea2.cpp	
---
+++ tarea2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    vector matriz
+    return 0;
+}
